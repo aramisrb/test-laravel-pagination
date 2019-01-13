1ee369e8247bf4f0cde0e024c968e5583e0c0943
aramis <aramis900915@gmail.com>
2019-01-13 21:35:56 +0100

    pagination con botones

--- resources/js/components/AppComponent.vue
+++ resources/js/components/AppComponent.vue
@@ -2,7 +2,8 @@
 
     <div id="app">
         <h1>{{ message }}</h1>
-        <table>
+
+<table>
             <tbody>
                 <tr v-for="(project, index) in projects">
                     <td>{{ project.id }}</td>
@@ -12,25 +13,34 @@
             </tbody>
         </table>
         <div class="pagination">
-            <button class="btn btn-default" v-on:click="fetchPaginateProjects(pagination.prev_page_url)" :disabled="!pagination.prev_page_url">
-                Previous
-            </button>
 
             <span>Page {{ pagination.current_page}} of {{ pagination.last_page}}</span>
+            <v-pagination 
+            v-model=pagination.current_page 
+            :page-count=pagination.last_page
+            :labels="paginationAnchorTexts"
+            @input="next"
+            >
+            </v-pagination>
 
-            <button class="btn btn-default" v-on:click="fetchPaginateProjects(pagination.next_page_url)" :disabled="!pagination.next_page_url">
-                Next
-            </button>
+
         </div>
+        
+
     </div>
 </template>
 
 <script>
 
+import vPagination from 'vue-plain-pagination'
+
 export default {        
 
+        components: { vPagination },  
         
         data(){
+
+
             return {
                 projects: [],
                 project: {
@@ -41,7 +51,13 @@ export default {
                 url: "api/projects",
                 pagination: [],
                 message:'Hello concha',
-                currentPage:'1'
+                currentPage:'1',
+                paginationAnchorTexts: {
+                    first: 'First',
+                    prev: 'Previous',
+                    next: 'Next',
+                    last: 'Last'
+                }
             }
         },
         mounted() {
@@ -62,12 +78,16 @@ export default {
                     last_page: data.last_page,
                     next_page_url: data.next_page_url,
                     prev_page_url: data.prev_page_url
+                    
                 }
                 this.pagination = pagination
             },
             fetchPaginateProjects(url) {
                 this.url=url
                 this.getProjects()
+            },next(page) {
+                this.url="api/projects?page="+page
+                this.getProjects()                
             }
         }
 
